245010ee6ef7adc936150780b88c0dda743b7564
jlz <ploskiy.mir000@gmail.com>
2023-09-15 10:18:42 +0300

    9 11 was created correct main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import runpy
+
+runpy.run_path('tasks/task_1.py')
+runpy.run_path('tasks/task_3.py')
+runpy.run_path('tasks/task_4.py')
+runpy.run_path('tasks/task_5.py')
+runpy.run_path('tasks/task_6.py')
+runpy.run_path('tasks/task_7.py')
+
